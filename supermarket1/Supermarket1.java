4fc088db0a133afa4d5a38d70a3540c8482aad33
Masa Hassan Masri <93089580+masamasri01@users.noreply.github.com>
2022-09-15 19:58:28 +0300

    Add files via upload

---
+++ supermarket1/Supermarket1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package supermarket1;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author EliteBook
+ */
+public class Supermarket1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        mainf mf=new mainf();
+mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mf.setSize(880,600);
+		mf.setVisible(true);
+
+    }
+    
+}
